ea2a3497cb0c19454fe889d08b0ffd92f016e6f2
Helton Alves <helton99@hotmail.com>
2022-01-21 17:15:45 -0300

    Added dist build directory.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: './node_modules/@surmon-china/abc-factory/preset/eslintrc/vue.typescript',
   rules: {
     'vue/max-attributes-per-line': 0,
-    'vue/order-in-components': 0
+    'vue/order-in-components': 0,
+    'vue/require-slots-as-functions': 0,
   }
 }
